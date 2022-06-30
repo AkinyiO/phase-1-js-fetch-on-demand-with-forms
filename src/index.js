983bfc8947fbcf919ca00fd5ccd613acad922697
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-06-30 16:17:02 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const init = () => {
       fetch(`http://localhost:3000/movies/${input.value}`)
       .then(response => response.json())
       .then(data => {
+
         const title = document.querySelector('section#movieDetails h4');
         const summary = document.querySelector('section#movieDetails p');
   
